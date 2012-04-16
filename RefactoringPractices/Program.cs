62a4461f0ed93aead9878f18eb89878744d6a36c
anshul1806 <anshul1806@gmail.com>
2012-04-15 21:35:29 -0700

    First working draft ready

--- RefactoringPractices/Program.cs
+++ RefactoringPractices/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using RefactoringPractices.MovieClasses;
 
 namespace RefactoringPractices
 {
@@ -9,6 +10,19 @@ namespace RefactoringPractices
     {
         static void Main(string[] args)
         {
+            Movie t12 = new Movie("Titanic 2012", "1");
+            Movie HF2 = new Movie("Housefull 2 2010", "0");
+            Movie chump= new Movie("Chimpanjee", "2");
+            Movie shuarya = new Movie("Shaurya", "0");
+            Rental titanicR = new Rental(t12, 2);
+            Rental hf2R = new Rental(HF2, 3);
+            Rental chimpanzeeR = new Rental(chump, 2);
+            Customer anshul = new Customer("Anshul Pandey");
+            anshul.AddRental(titanicR);
+            anshul.AddRental(hf2R);
+            anshul.AddRental(chimpanzeeR);
+            anshul.PringStatement();
+            Console.ReadLine();
         }
     }
 }
