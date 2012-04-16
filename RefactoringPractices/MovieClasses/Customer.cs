99e2164cbfa45efbe12a99a600238b165f258bb2
anshul1806 <anshul1806@gmail.com>
2012-04-15 19:51:09 -0700

    add rental vector

--- RefactoringPractices/MovieClasses/Customer.cs
+++ RefactoringPractices/MovieClasses/Customer.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections.Generic;
 
 namespace RefactoringPractices.MovieClasses
 {
     class Customer
     {
         public String Name { get; set; }
+        public List<Rental> rentals = new List<Rental>();
+        
     }
 }
